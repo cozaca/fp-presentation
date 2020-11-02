161672463e00b935c3f00539423bc7426cd6559c
Adrian Cozac <adrian.cozac@itiviti.com>
2020-11-02 17:16:15 +0200

    Add Functor instances and tests
    Change-Id: I37186d8ce780e5f25baf0d3419a5f7ebbd40141b

--- src/main/kotlin/me/fp/presentation/functor/impl/FunctorInstances.kt
+++ src/main/kotlin/me/fp/presentation/functor/impl/FunctorInstances.kt
@@ -16,14 +16,40 @@ data class Just<A>(val a: A) : Maybe<A>()
 object Empty : Maybe<Nothing>()
 
 
+class ForMyList private constructor() {
+    companion object
+}
+typealias MyListOf<A> = arrow.Kind<ForMyList, A>
+
+@Suppress("UNCHECKED_CAST", "NOTHING_TO_INLINE")
+inline fun <A> MyListOf<A>.fix(): MyList<A> =
+        this as MyList<A>
+
+sealed class MyList<A> : MyListOf<A>
+data class Cons<A>(val head: A, val tail: MyListOf<A>) : MyListOf<A>
+object Nill : MyListOf<Nothing>
+
+
 class FunctorInstances {
+
     fun <A> maybeFunctor(): Functor<ForMaybe, A> = object : Functor<ForMaybe, A> {
         override fun <A, B> map(box: MaybeOf<A>, f: (A) -> B): MaybeOf<B> {
-            return when {
-                box is Just -> Just(f(box.a))
-                box is Empty -> box
+            return when (box) {
+                is Just -> Just(f(box.a))
+                is Empty -> box
                 else -> Empty
             }
         }
     }
+
+    fun <A> myListFunctor(): Functor<ForMyList, A> = object : Functor<ForMyList, A> {
+        override fun <A, B> map(box: MyListOf<A>, f: (A) -> B): MyListOf<B> {
+            return when (box) {
+                is Cons -> Cons(f(box.head), map(box.tail, f))
+                is Nill -> box
+                else -> Nill
+            }
+        }
+    }
+
 }
--- src/test/groovy/me/fp/presentation/functor/FunctorSpec.groovy
+++ src/test/groovy/me/fp/presentation/functor/FunctorSpec.groovy
@@ -1,12 +1,18 @@
 package me.fp.presentation.functor
 
+import me.fp.presentation.functor.impl.Cons
 import me.fp.presentation.functor.impl.Empty
 import me.fp.presentation.functor.impl.FunctorInstances
 import me.fp.presentation.functor.impl.Just
+import me.fp.presentation.functor.impl.Nill
+import spock.genesis.Gen
 import spock.lang.Specification
+import spock.lang.Unroll
 
+@Unroll
 class FunctorSpec extends Specification {
     def functor = new FunctorInstances().<Integer> maybeFunctor()
+    def myListFunctor = new FunctorInstances().<Integer> myListFunctor()
 
     def 'map maybe value with functor implementation'() {
         expect:
@@ -20,5 +26,39 @@ class FunctorSpec extends Specification {
         functor.map(a, { value -> value.toString() }) == a
         where:
         a << new Empty()
+    }
+
+    def 'maybe identity'() {
+        expect:
+        functor.map(F, { a  -> a} ) == F
+
+        where:
+        F << Gen.these(-50,50).then(Gen.integer).map {new Just<Integer>(it)}.take(100)
+    }
+
+    def 'maybe associativity'() {
+        expect:
+        functor.map(functor.map(F, {it * 5}), {it * 10}) == functor.map(functor.map(F, {it * 10}), {it * 5}) //
+        functor.map(functor.map(F, {it * 5}), {it * 10}) == functor.map(F, {it * 5 * 10})
+
+        where:
+        F << Gen.these(-50,50).then(Gen.integer).map {new Just<Integer>(it)}.take(100)
+    }
+
+    def 'list identity'() {
+        expect:
+        myListFunctor.map(F, { a  -> a} ) == F
+
+        where:
+        F << Gen.these(-50,50).then(Gen.integer).map {new Cons<Integer>(it, new Cons<Integer>(it, new Nill()))}.take(100)
+    }
+
+    def 'list associativity'() {
+        expect:
+        functor.map(functor.map(F, {it * 5}), {it * 10}) == functor.map(functor.map(F, {it * 10}), {it * 5}) //
+        functor.map(functor.map(F, {it * 5}), {it * 10}) == functor.map(F, {it * 5 * 10})
+
+        where:
+        F << Gen.these(-50,50).then(Gen.integer).map {new Cons<Integer>(it, new Cons<Integer>(it, new Nill()))}.take(100)
     }
 }
